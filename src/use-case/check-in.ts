b43e6b1e47236d89b789d23544ee21c1e8e01509
Tiago Martins <tiago_martins238@hotmail.com>
2023-06-20 20:26:12 -0300

    Chekins

---
+++ src/use-case/check-in.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+export class CheckinUseCase {
+  constructor(private checkInRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
